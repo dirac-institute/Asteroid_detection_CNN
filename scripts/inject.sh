ef87feec87cd168ac932e9567be3772a0cfbfb1a
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2024-05-27 14:39:28 +0200

    Changed inject script to allow custom path definition

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -7,39 +7,42 @@ if [ $# -lt 1 ]; then
 fi
 RUN_NUM=$1
 
-# LSST stack activation, change the paths to your own
-source ~/lsst_stack/loadLSST.sh
+# LSST stack activation
+source $LSST_STACK_PATH
 setup lsst_distrib
-setup -k -r ~/lsst_stack/source_injection
-setup -j -r /epyc/ssd/users/kmrakovc/DATA/rc2_subset
-REPO="$RC2_SUBSET_DIR/SMALL_HSC/butler.yaml"
-export REPO
-BASE_DIR="/astro/users/kmrakovc/Projects/LSST_Streak_Detection"
+setup source_injection
 echo -e "\nLSST stack activated\n"
 start_time=$(date +%s)
 
 
 # Make pipeline with injection
-make_injection_pipeline -t postISRCCD -r $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset.yaml -f $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset_injection.yaml --overwrite
-pipetask build -p $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset_injection.yaml -c inject_exposure:selection="np.isin(injection_catalog['visit'], {visit})" -s $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset_injection.yaml
+make_injection_pipeline -t postISRCCD -r $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset.yaml \
+-f $PROJECT_PATH/DATA/DRP-RC2_subset_injection.yaml --overwrite
+pipetask build -p $PROJECT_PATH/DATA/DRP-RC2_subset_injection.yaml \
+-c inject_exposure:selection="np.isin(injection_catalog['visit'], {visit})" \
+-s $PROJECT_PATH/DATA/DRP-RC2_subset_injection.yaml
 echo -e "\nPipeline with injection created\n"
 
 # Generate injection catalog
-python3 $BASE_DIR/tools/generate_injection_catalog.py \
--r $REPO \
--i u/$USER/RC2_subset/run_1 \
--o u/$USER/injection_inputs_$RUN_NUM \
+python3 $PROJECT_PATH/tools/generate_injection_catalog.py \
+-r $REPO_PATH \
+-i $INPUT_COLL \
+-o $OUTPUT_COLL/injection_inputs_$RUN_NUM \
 -n 20 \
 -l 4 74 \
--m 25.0 25.5 \
+-m 20.0 25.5 \
 -b 0.00 180.0
-# this is the old way, do not use it
-#generate_injection_catalog -a 149.8 150.7 -d 1.99 2.5 -n 2 -p source_type Trail -p trail_length 1 -p mag 15 17 19 21 23 25 27 -p beta 0 20 40 50 70 80 -w calexp -c u/$USER/RC2_subset/run_1 -b $REPO -i g r i z y -o u/$USER/injection_inputs_$RUN_NUM
 echo -e "\nInjection catalog generated\n"
 
 # Run the pipeline with injection
-rm ~/inject_$RUN_NUM.txt run
-pipetask --long-log --log-file ~/inject_$RUN_NUM.txt run --register-dataset-types -b $REPO -i u/$USER/RC2_subset/run_1,u/$USER/injection_inputs_$RUN_NUM -o u/$USER/single_frame_injection_$RUN_NUM -p $DRP_PIPE_DIR/pipelines/HSC/DRP-RC2_subset_injection.yaml#nightlyStep1 -j 16 -c inject_exposure:process_all_data_ids=True
+rm ~/inject_log_$RUN_NUM.txt
+pipetask --log-file ~/inject_log_$RUN_NUM.txt run --register-dataset-types \
+-b $REPO_PATH \
+-i $INPUT_COLL, $OUTPUT_COLL/injection_inputs_$RUN_NUM, $VISIT_SUMMARY \
+-o $OUTPUT_COLL/single_frame_injection_$RUN_NUM \
+-p $PROJECT_PATH/DATA/DRP-RC2_subset_injection.yaml#nightlyStep1 \
+-j 16 \
+-c inject_exposure:process_all_data_ids=True
 echo -e "\nFINISHED\n"
 
 # Capture the end time and calculate the total runtime.
