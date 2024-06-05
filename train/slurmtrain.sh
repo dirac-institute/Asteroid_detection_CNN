a853c6abd9ac610e0f2788e8c778835a6c99afa3
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2024-06-05 16:23:35 +0200

    Added model evaluation program

--- train/slurmtrain.sh
+++ train/slurmtrain.sh
@@ -4,28 +4,26 @@
 #SBATCH --mail-type=ALL
 #SBATCH --account=escience
 #SBATCH --output=/mmfs1/home/kmrakovc/Results/Asteroids/training_%j.txt
-#SBATCH --partition=gpu-rtx6k
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:2
-#SBATCH --time=72:00:00
+#SBATCH --partition=gpu-a40
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:6
+#SBATCH --time=5-00:00:00
 
 source ~/activate.sh
 module load cuda/12.3.2
 srun python3 main.py \
 --train_dataset_path ../DATA/train1.tfrecord \
 --test_dataset_path ../DATA/test1.tfrecord \
---arhitecture ../DATA/arhitecture_tuned.json \
+--arhitecture ../arhitecture.json \
 --model_destination ../DATA/Trained_model_$SLURM_JOB_ID \
 --no-multiworker \
---kernel_size 3 \
+--kernel_size 7 \
 --merge_operation "concat" \
---epochs 512 \
---alpha 0.9 \
---gamma 3 \
+--epochs 1024 \
+--alpha 0.99 \
+--gamma 3.1 \
 --batch_size 256 \
 --start_lr 0.001 \
 --decay_lr_rate 0.75 \
---decay_lr_patience 8 \
+--decay_lr_patience 10 \
 --no-verbose
