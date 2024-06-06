c59448ebe2dbccc6ee5fa375422742cbc02d4aa9
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2024-06-06 10:54:22 +0200

    Added model evaluation program

--- experiments/smag_len_hist.sh
+++ experiments/smag_len_hist.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=escience
 #SBATCH --output=/mmfs1/home/kmrakovc/Results/Asteroids/evaluating.txt
 #SBATCH --partition=gpu-rtx6k
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=9
 #SBATCH --gres=gpu:2
 #SBATCH --time=24:00:00
 
@@ -13,4 +13,5 @@ source ~/activate.sh
 python3 mag_len_hist.py \
 --model_path ../DATA/Trained_model_18796700.keras \
 --tf_dataset_path ../DATA/test1.tfrecord \
---collection u/kmrakovc/single_frame_injection_01
+--collection u/kmrakovc/single_frame_injection_01 \
+--cpu_count $SLURM_CPUS_PER_TASK
