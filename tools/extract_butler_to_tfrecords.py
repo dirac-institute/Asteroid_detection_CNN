87f3b9fba08103c01662fc5d75ff654b83b0b829
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2024-06-23 21:25:10 +0200

    Added tfdata train set creation from butler

---
+++ tools/extract_butler_to_tfrecords.py
@@ -0,0 +1,33 @@
+import data
+import numpy as np
+import argparse
+import sys
+
+
+def main(args):
+    val_index = tools.data.convert_butler_tfrecords(args.repo, args.coll, shape=(128, 128),
+                                                    filename_train=args.filename_train,
+                                                    filename_test=args.filename_test,
+                                                    train_split=args.split,
+                                                    verbose=True,
+                                                    seed=args.seed)
+    val_index = np.array(val_index)
+    val_index.sort()
+    with open(args.filename_index, 'wb') as f:
+        np.save(f, val_index)
+
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--repo", type=str, help="Path to the repo", required=True)
+    parser.add_argument("--coll", type=str, help="Name of the collection", required=True)
+    parser.add_argument("--filename_train", type=str, help="Filename of the train dataset", required=True)
+    parser.add_argument("--filename_test", type=str, help="Filename of the test dataset", required=True)
+    parser.add_argument("--filename_index", type=str, help="Filename of the index", required=True)
+    parser.add_argument("--split", type=float, help="Split ratio", default=0.25)
+    parser.add_argument("--seed", type=int, help="Seed for random split", default=42)
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    main(parse_arguments(sys.argv[1:]))
