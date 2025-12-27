69b56757538ef427d1d5fbfa8cbb9e73a6a805ff
karlo <mrakovcickarlo@gmail.com>
2025-12-27 18:42:39 +0100

    Update argument parsing in idea2.py: add validation fraction and pin memory options, modify save paths

--- experiments/idea1.sh
+++ experiments/idea1.sh
@@ -4,9 +4,9 @@
 #SBATCH --account kipac:kipac
 #SBATCH --partition ada
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=60G
 #SBATCH --time=5-00:00:00
 #SBATCH --output=/sdf/home/m/mrakovci/logs/%x.out
